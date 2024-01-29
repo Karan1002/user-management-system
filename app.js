314479163a367581271f51673f96fce7d25a1bdf
Karan1002 <karankushwaha7227@gmail.com>
2024-01-29 23:37:47 +0530

    setup

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const expressEjsLayouts = require("express-ejs-layouts");
+const app = express();
+
+const port = 3000 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static files      html css js
+app.use(express.static("public"));
+
+//templating engine
+app.use(expressEjsLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const locals = {
+    title: "User Management System",
+    description: "User Management System Node JS",
+  };
+  res.render("index", { locals });
+});
+
+app.listen(port, () => {
+  console.log(`App connect with port ${port}`);
+});
