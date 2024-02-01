69ac517eb0475d7a9b826740ff092c4d4f1024a1
Karan1002 <karankushwaha7227@gmail.com>
2024-02-01 22:04:44 +0530

    feat: added some changes fix versal work

--- app.js
+++ app.js
@@ -39,6 +39,34 @@ app.use(expressEjsLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
+const whitelist = ["*"];
+
+app.use((req, res, next) => {
+  const origin = req.get("referer");
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === "OPTIONS") res.sendStatus(200);
+  else next();
+});
+
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
 //home route path
 app.use("/", require("./server/routes/customerRoutes"));
 
